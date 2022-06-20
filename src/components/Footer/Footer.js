a67311b437d19bf93b0e43242dd00bad87da149e
happyeveryone96 <happyeveryone@naver.com>
2022-06-20 16:12:22 +0900

    Add: 프론트엔드 초기 설정

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 import css from './Footer.module.scss';
 
 function Footer() {
-  return <div className={css.container}></div>;
+  return <div className={css.container}>Footer</div>;
 }
 
 export default Footer;
--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from '../pages/Login/Login';
+import Signup from '../pages/Signup/Signup';
+import Home from '../pages/Home/Home';
+import Products from '../pages/Products/Products';
+import ProductDetail from '../pages/ProductDetail/ProductDetail';
 
-import Home from './Home/Home';
-import Login from './Login/Login';
-import Signup from './Signup/Signup';
-
-function Router() {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/productdetail" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default Router;
+export default App;
