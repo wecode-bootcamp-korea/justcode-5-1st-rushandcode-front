01ed3402b4a5b63e8a81862fea2d2e6c0cd6f67d
YSBINN <dowkdg@naver.com>
2022-06-28 02:08:45 +0900

    Add : 메인페이지 배너 판매순으로 데이터받아서 보여주는기능 추가

--- src/components/Home/Second/Second.js
+++ src/components/Home/Second/Second.js
@@ -1,8 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import Secondmap from './Secondmap';
 import css from './Second.module.scss';
 
 function Second() {
   const slider = useRef();
+  const [productData, setProductData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:10010/products?sort=banner')
+      .then(res => res.json())
+      .then(res => setProductData(res.products));
+  }, []);
 
   const first_button = () => {
     slider.current.style.transform = 'translate(0vw)';
@@ -24,90 +32,15 @@ function Second() {
   return (
     <div className={css.wraper}>
       <div ref={slider} className={css.container}>
-        <div className={css.inner}>
-          <img
-            className={css.img}
-            src="https://www.lush.co.kr/data/goods/11/01/20/38/38_main_032.jpg"
-            alt="사진"
+        {productData.map(productData => (
+          <Secondmap
+            key={productData.id}
+            name={productData.name}
+            price={productData.price}
+            hashtags={productData.hashtags}
+            url={productData.productImages[0].url}
           />
-        </div>
-        <div className={css.inner}>
-          <img
-            className={css.img}
-            src="https://www.lush.co.kr/data/goods/15/01/07/152/152_main_065.jpg"
-            alt="사진"
-          />
-        </div>
-        <div className={css.inner}>
-          <img
-            className={css.img}
-            src="https://www.lush.co.kr/data/goods/11/01/20/38/38_main_032.jpg"
-            alt="사진"
-          />
-        </div>
-        <div className={css.inner}>
-          <img
-            className={css.img}
-            src="https://www.lush.co.kr/data/goods/11/01/20/15/15_main_020.jpg"
-            alt="사진"
-          />
-        </div>
-        <div className={css.inner}>
-          <img
-            className={css.img}
-            src="https://www.lush.co.kr/data/goods/11/01/25/88/88_main_058.jpg"
-            alt="사진"
-          />
-        </div>
-        <div className={css.inner}>
-          <img
-            className={css.img}
-            src="https://www.lush.co.kr/data/goods/11/01/20/41/41_main_088.jpg"
-            alt="사진"
-          />
-        </div>
-        <div className={css.inner}>
-          <img
-            className={css.img}
-            src="https://www.lush.co.kr/data/goods/11/01/20/76/76_main_063.jpg"
-            alt="사진"
-          />
-        </div>
-        <div className={css.inner}>
-          <img
-            className={css.img}
-            src="https://www.lush.co.kr/data/goods/17/08/16/246/246_main_060.jpg"
-            alt="사진"
-          />
-        </div>
-        <div className={css.inner}>
-          <img
-            className={css.img}
-            src="https://www.lush.co.kr/data/goods/12/03/07/97/97_main_074.jpg"
-            alt="사진"
-          />
-        </div>
-        <div className={css.inner}>
-          <img
-            className={css.img}
-            src="https://www.lush.co.kr/data/goods/22/04/14/1000002127/1000002127_main_063.png"
-            alt="사진"
-          />
-        </div>
-        <div className={css.inner}>
-          <img
-            className={css.img}
-            src="https://www.lush.co.kr/data/goods/22/04/14/1000002126/1000002126_main_0100.png"
-            alt="사진"
-          />
-        </div>
-        <div className={css.inner}>
-          <img
-            className={css.img}
-            src="https://www.lush.co.kr/data/goods/22/04/14/1000002135/1000002135_main_078.jpg"
-            alt="사진"
-          />
-        </div>
+        ))}
       </div>
       <div className={css.wrap_button}>
         <button className={css.button} onClick={first_button} />
