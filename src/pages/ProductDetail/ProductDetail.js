d5e65e1e70109d7db63ba3d43ced1aa22d4368c5
happyeveryone96 <happyeveryone@naver.com>
2022-06-29 08:38:51 +0900

    Refactor: 불필요한 코드 제거

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import Image from '../../elements/Image';
 import ProductInfo from '../../components/ProductInfo/ProductInfo';
