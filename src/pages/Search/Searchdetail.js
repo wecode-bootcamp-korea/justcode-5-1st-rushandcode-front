63595df1169e4af484de05b364fbf13a4ad759b7
YSBINN <dowkdg@naver.com>
2022-07-06 16:14:27 +0900

    Add : 검색창 기능구현

--- src/pages/Search/Searchdetail.js
+++ src/pages/Search/Searchdetail.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import css from './Searchdetail.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const Searchdetail = ({ data }) => {
+  const navigate = useNavigate();
+
+  const gotoproduct = () => {
+    navigate(`/productdetail/${data.id}`);
+  };
+
   return (
     <div className={css.product}>
-      <img className={css.img} src={data.productImages[0].url} />
-      <div className={css.wrapinfo}>
-        <p>{data.name}</p>
-        <p> {data.hashtags}</p>
-        <p>{data.price}</p>
-      </div>
+      <img
+        className={css.img}
+        src={data.productImages[0].url}
+        onClick={gotoproduct}
+      />
+      <p className={css.info}>{data.name}</p>
+      <p className={css.info}> {data.hashtags}</p>
+      <p className={css.info}>{data.price}</p>
     </div>
   );
 };
