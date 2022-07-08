fb08a6f9c6ae2a92edbebbb7a9f41da7c0c70257
ssm825 <ssm39825@gmail.com>
2022-07-08 15:45:16 +0900

    Refactor: 리뷰 반영, 중복되는 코드 정리

--- src/components/ProductsCategory/ProductsCategory.js
+++ src/components/ProductsCategory/ProductsCategory.js
@@ -1,178 +1,88 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import css from './ProductsCategory.module.scss';
 
-function ProductCategory(props) {
-  const { mainCategory, subCategory } = props;
+function CategoryItem(props) {
+  const { to, subCategory } = props;
 
+  let query = new URLSearchParams(useLocation().search);
+  let sub = query.get('subCategory');
+
+  return (
+    <>
+      {subCategory === null && (
+        <li>
+          <Link to={to}>
+            <span className={sub === null ? css.on : undefined}>전체(36)</span>
+          </Link>
+        </li>
+      )}
+      {subCategory && (
+        <li>
+          <Link to={to}>
+            <span className={subCategory === sub ? css.on : undefined}>
+              {subCategory}(12)
+            </span>
+          </Link>
+        </li>
+      )}
+    </>
+  );
+}
+
+function ProductCategory(props) {
+  const { mainCategory } = props;
+  const mainURL = `/products?mainCategory=${mainCategory}`;
+  const prdCategory = [
+    {
+      maincategory: '배쓰',
+      subcategory: ['배쓰 밤', '버블 바', '배쓰 오일'],
+    },
+    {
+      maincategory: '샤워',
+      subcategory: ['솝', '샤워 젤', '보디 컨디셔너'],
+    },
+    {
+      maincategory: '보디',
+      subcategory: ['클렌저', '로션', '핸드 앤 풋'],
+    },
+  ];
   return (
     <>
       {mainCategory === '배쓰' && (
         <ul className={css.list_category}>
-          <li>
-            <Link to="/products?mainCategory=배쓰">
-              <span
-                className={
-                  mainCategory === '배쓰' && subCategory === null
-                    ? css.on
-                    : undefined
-                }
-              >
-                전체(36)
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/products?mainCategory=배쓰&subCategory=배쓰 밤">
-              <span
-                className={
-                  mainCategory === '배쓰' && subCategory === '배쓰 밤'
-                    ? css.on
-                    : undefined
-                }
-              >
-                배쓰 밤(12)
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/products?mainCategory=배쓰&subCategory=버블 바">
-              <span
-                className={
-                  mainCategory === '배쓰' && subCategory === '버블 바'
-                    ? css.on
-                    : undefined
-                }
-              >
-                버블 바(12)
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/products?mainCategory=배쓰&subCategory=배쓰 오일">
-              <span
-                className={
-                  mainCategory === '배쓰' && subCategory === '배쓰 오일'
-                    ? css.on
-                    : undefined
-                }
-              >
-                배쓰 오일(12)
-              </span>
-            </Link>
-          </li>
+          <CategoryItem to={mainURL} subCategory={null} />
+          {prdCategory[0].subcategory.map(data => (
+            <CategoryItem
+              key={data}
+              to={`${mainURL}&subCategory=${data}`}
+              subCategory={data}
+            />
+          ))}
         </ul>
       )}
       {mainCategory === '샤워' && (
         <ul className={css.list_category}>
-          <li>
-            <Link to="/products?mainCategory=샤워">
-              <span
-                className={
-                  mainCategory === '샤워' && subCategory === null
-                    ? css.on
-                    : undefined
-                }
-              >
-                전체(36)
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/products?mainCategory=샤워&subCategory=솝">
-              <span
-                className={
-                  mainCategory === '샤워' && subCategory === '솝'
-                    ? css.on
-                    : undefined
-                }
-              >
-                솝(12)
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/products?mainCategory=샤워&subCategory=샤워 젤">
-              <span
-                className={
-                  mainCategory === '샤워' && subCategory === '샤워 젤'
-                    ? css.on
-                    : undefined
-                }
-              >
-                샤워 젤(12)
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/products?mainCategory=샤워&subCategory=보디 컨디셔너">
-              <span
-                className={
-                  mainCategory === '샤워' && subCategory === '보디 컨디셔너'
-                    ? css.on
-                    : undefined
-                }
-              >
-                보디 컨디셔너(12)
-              </span>
-            </Link>
-          </li>
+          <CategoryItem to={`${mainURL}`} subCategory={null} />
+          {prdCategory[1].subcategory.map(data => (
+            <CategoryItem
+              key={data}
+              to={`${mainURL}&subCategory=${data}`}
+              subCategory={data}
+            />
+          ))}
         </ul>
       )}
       {mainCategory === '보디' && (
         <ul className={css.list_category}>
-          <li>
-            <Link to="/products?mainCategory=보디">
-              <span
-                className={
-                  mainCategory === '보디' && subCategory === null
-                    ? css.on
-                    : undefined
-                }
-              >
-                전체(36)
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/products?mainCategory=보디&subCategory=클렌저">
-              <span
-                className={
-                  mainCategory === '보디' && subCategory === '클렌저'
-                    ? css.on
-                    : undefined
-                }
-              >
-                클렌저(12)
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/products?mainCategory=보디&subCategory=로션">
-              <span
-                className={
-                  mainCategory === '보디' && subCategory === '로션'
-                    ? css.on
-                    : undefined
-                }
-              >
-                로션(12)
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/products?mainCategory=보디&subCategory=핸드 앤 풋">
-              <span
-                className={
-                  mainCategory === '보디' && subCategory === '핸드 앤 풋'
-                    ? css.on
-                    : undefined
-                }
-              >
-                핸드 앤 풋(12)
-              </span>
-            </Link>
-          </li>
+          <CategoryItem to={`${mainURL}`} subCategory={null} />
+          {prdCategory[2].subcategory.map(data => (
+            <CategoryItem
+              key={data}
+              to={`${mainURL}&subCategory=${data}`}
+              subCategory={data}
+            />
+          ))}
         </ul>
       )}
     </>
